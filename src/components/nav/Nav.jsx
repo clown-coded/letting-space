d28bceef810cda556da595e6f021ec55a5283860
sammitchell <mitchesama1@myvuw.ac.nz>
2023-05-25 15:54:10 +1200

    fix again

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -165,7 +165,7 @@ const Nav = () => {
                   }}/>
                   <div className='logo-container'>
                       <Link to='/'>
-                          <img className='logo' src="../../../public/assets/letting-space-logo.png" alt="Letting Space Logo" onClick={() => { setCurrentPage('home') }} /> </Link>
+                          <img className='logo' src="../../../public/assets/letting-space-logo.jpg" alt="Letting Space Logo" onClick={() => { setCurrentPage('home') }} /> </Link>
                       
                       <ChevronDown onClick={toggleMenu}
                             id= 'open-menu' className= {menuIsOpen ? 'rotate' : 'unrotate' } 
